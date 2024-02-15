a1c87aef112ed648aaf0317bf42fbb156959eaef
Sam Mirosh <sammirosh@gmail.com>
2024-02-15 20:25:39 +0300

    init 1.1: Project initialization

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new webpack.ProgressPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
+
+export default config;
